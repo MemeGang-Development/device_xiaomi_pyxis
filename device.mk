6b7cb0d37170ca44aaf15379a3f7a8021d3715a6
Ivan Vecera <ivan@cera.cz>
2021-03-16 17:23:50 +0100

    pyxis: move IR to common tree
    All sdm710 devices have IR support, so we are defining that in common.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I44690e67ffee98cd782dd47ccdff5558fd4064c0
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- device.mk
+++ device.mk
@@ -90,10 +90,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
-# IR
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi
-
 # Native libraries whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
